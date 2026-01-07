a00b2867c73cc58d492cfd2ddfad345030900d6e
spotdemo4 <me@trev.xyz>
2026-01-07 14:45:20 -0500

    fix: small style changes

--- run.sh
+++ run.sh
@@ -6,24 +6,23 @@ normal=$(tput -T xterm sgr0)
 SESSIONS=$(sudo lsof -i :22 | wc -l)
 TS_HOST=$(tailscale status | head -1 | awk '{print $2}')
 echo " "
-echo "connect: ${bold}ssh $(whoami)@${TS_HOST}${normal}"
+echo "Connect: ${bold}ssh $(whoami)@${TS_HOST}${normal}"
 echo " "
 
-echo "waiting for a connection..."
-journalctl -f -u ssh.service &
+journalctl -f -u ssh.service -o cat &
 LOGGER=$!
 
 until [[ "$(sudo lsof -i :22 | wc -l)" -gt "${SESSIONS}" ]]; do
     sleep 10s
 done
 
-echo "${bold}connected!${normal}"
-echo "will stop after five minutes of inactivity"
+echo "${bold}Connected!${normal} Will stop after five minutes of inactivity"
 
 INACTIVE=0
 until [[ "${INACTIVE}" -ge "5" ]]; do
     if [[ "$(sudo lsof -i :22 | wc -l)" -le "${SESSIONS}" ]]; then
         ((INACTIVE++))
+        echo "Inactive for ${INACTIVE}/5 minutes"
     else
         INACTIVE=0
     fi
